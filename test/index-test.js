1573f9ad2de79fb6641a4e1fa15125f33991b5f8
Josh Perez <josh@goatslacker.com>
2012-05-24 16:31:17 -0700

    Modifieds unit tests to work with new gister api v3

--- test/index-test.js
+++ test/index-test.js
@@ -6,12 +6,9 @@ var request = require('./mock/request')
 
 var Gist = require('../gister')
 
-function newgist(id) {
-  var gist = new Gist({
-    gist_id: id,
-    username: "octocat",
-    token: "secret"
-  })
+function clone_gist(opts) {
+  opts = opts || { gist_id: 1, username: 'octocat', token: 'secret' }
+  var gist = new Gist(opts)
   gist.request = request
   return gist
 }
@@ -22,66 +19,82 @@ function wrap(cb) {
   }
 }
 
-vows.describe("gister").addBatch({
-  "when getting a gist": {
+function error(msg) {
+  return function (err) {
+    assert.isTrue(err instanceof Error)
+    assert.equal(err.message, msg)
+  }
+}
 
-    "and not providing a gist id": {
+function ok(body) {
+  return function (topic) {
+    assert.deepEqual(body, topic)
+  }
+}
+
+var sample_gist = {
+  'ring.erl': 'contents of gist'
+}
+
+vows.describe('gister').addBatch({
+  'when getting a gist': {
+
+    'and not providing a gist id': {
       topic: function () {
-        var gist = newgist()
-        gist.on('error:gist_id', wrap(this.callback))
+        var gist = clone_gist()
+        gist.gist_id = null
+        gist.on('error', wrap(this.callback))
         gist.get()
       },
 
-      "should receive an error": function (err) {
-        assert.equal(err, 'No gist id provided')
-      }
+      'should receive an error': error('No gist id provided')
     },
 
-    "by gist id": {
+    'by gist id': {
       topic: function () {
-        var gist = newgist(1)
-        gist.on('get', wrap(this.callback))
+        var gist = clone_gist({ gist_id: 1 })
+        gist.on('gist', wrap(this.callback))
         gist.get()
       },
 
-      "should return the body": function (topic) {
-        assert.deepEqual(topic, response.get)
-      }
+      'should return the body': ok(response.get)
     },
 
-    "and an error is returned from request": {
+    'and an error is returned from request': {
+      // FIXME
       topic: function () {
         var gist = new Gist({ gist_id: 1 })
         gist.request = function (statusCode, cb) {
           cb(new Error(), { statusCode: 200, headers: {} }, {})
         }
-        gist.on('err', wrap(this.callback))
+        gist.on('error', wrap(this.callback))
         gist.get()
       },
 
-      "should call error event": function (err) {
+      'should call error event': function (err) {
         assert.isTrue(err instanceof Error)
       }
     },
 
-    "and the response statusCode is not found 404": {
+    'and the response statusCode is not found 404': {
+      // FIXME
       topic: function () {
         var gist = new Gist({ gist_id: 1 })
         gist.request = function (statusCode, cb) {
           cb(null, { statusCode: 404, headers: {} }, {})
         }
-        gist.on('error:notfound', wrap(this.callback))
+        gist.on('error', wrap(this.callback))
         gist.get()
       },
 
-      "should call notfound event": function (err) {
+      'should call notfound event': function (err) {
         assert.isTrue(Boolean(err))
       }
     },
 
-    "and the response statusCode is 201": {
+    'and the response statusCode is 201': {
       topic: function () {
-        var gist = newgist()
+        var gist = clone_gist()
         gist.request = function (statusCode, cb) {
           cb(null, { statusCode: 201, headers: {} }, {})
         }
@@ -89,13 +102,14 @@ vows.describe("gister").addBatch({
         gist.create()
       },
 
-      "should emit created event": function (err, body) {
+      'should emit created event': function (err, body) {
         assert.isTrue(Boolean(body))
       }
     },
 
-    "and the response statusCode is 500 internal server error": {
+    'and the response statusCode is 500 internal server error': {
       topic: function () {
+        // FIXME
         var gist = new Gist({ gist_id: 1 })
         gist.request = function (statusCode, cb) {
           cb(null, { statusCode: 500, headers: {} }, {})
@@ -104,131 +118,116 @@ vows.describe("gister").addBatch({
         gist.get()
       },
 
-      "should emit err event": function (err) {
+      'should emit err event': function (err) {
         assert.isTrue(Boolean(err))
       }
     }
   },
 
-  "when synchronizing the gist": {
+  'when synchronizing the gist': {
 
-    "and providing a gist id": {
+    'and providing a gist id': {
       topic: function () {
-        var gist = newgist(1)
-        gist.on('updated', wrap(this.callback))
-        gist.sync("contents of gist")
+        var gist = clone_gist({ token: 'secret' })
+        gist.on('edited', wrap(this.callback))
+        gist.sync(sample_gist, 1)
       },
 
-      "should edit existing gist": function (topic) {
-        assert.deepEqual(topic, response.patch)
-      }
+      'should edit existing gist': ok(response.patch)
     },
 
-    "with no gist id": {
+    'with no gist id': {
       topic: function () {
-        var gist = newgist()
+        var gist = clone_gist()
+        gist.gist_id = null
         gist.on('created', wrap(this.callback))
-        gist.sync("contents of gist")
+        gist.sync(sample_gist)
       },
 
-      "should create a new gist": function (err, topic, id) {
+      'should create a new gist': function (err, topic, id) {
         assert.deepEqual(topic, response.post)
-        assert.equal(id, "1")
+        assert.equal(id, '1')
       }
     }
   },
 
-  "when editing a gist": {
+  'when editing a gist': {
 
-    "and providing token and gist id": {
+    'and providing token and gist id': {
       topic: function () {
-        var gist = newgist(1)
-        gist.on('updated', wrap(this.callback))
-        gist.edit("contents of gist")
+        var gist = clone_gist({ gist_id: 1, token: 'secret' })
+        gist.on('edited', wrap(this.callback))
+        gist.edit(sample_gist)
       },
 
-      "should receive a response": function (topic) {
-        assert.deepEqual(topic, response.patch)
-      }
+      'should receive a response': ok(response.patch)
     },
 
-    "and not providing a gist id": {
+    'and not providing a gist id': {
       topic: function () {
-        var gist = newgist()
-        gist.on('error:gist_id', wrap(this.callback))
+        var gist = clone_gist()
+        gist.gist_id = null
+        gist.on('error', wrap(this.callback))
         gist.edit()
       },
 
-      "should receive an error": function (err) {
-        assert.equal(err, 'No gist id provided')
-      }
+      'should receive an error': error('No gist id provided')
     },
 
-    "and providing a username but no password": {
+    'and providing a username but no password': {
       topic: function () {
-        var gist = new Gist({ username: "octocat", gist_id: 1 })
-        gist.request = request
+        var gist = clone_gist({ username: 'octocat', gist_id: 1 })
         gist.on('error:credentials', wrap(this.callback))
-        gist.edit("contents of gist")
+        gist.edit(sample_gist)
       },
 
-      "should receive an error": function (err) {
-        assert.equal(err, 'No OAuth token or username and password provided')
-      }
+      'should receive an error': error('No OAuth token or username and password provided')
     },
 
-    "without providing a username, just a password": {
+    'without providing a username, just a password': {
       topic: function () {
-        var gist = new Gist({ password: "secret", gist_id: 1 })
+        var gist = clone_gist({ password: 'secret', gist_id: 1 })
         gist.request = request
         gist.on('error:credentials', wrap(this.callback))
-        gist.edit("contents of gist")
+        gist.edit(sample_gist)
       },
 
-      "should receive an error": function (err) {
-        assert.equal(err, 'No OAuth token or username and password provided')
-      }
+      'should receive an error': error('No OAuth token or username and password provided')
     }
   },
 
-  "when creating a new gist": {
+  'when creating a new gist': {
 
-    "and authenticated": {
+    'and authenticated': {
       topic: function () {
-        var gist = newgist()
+        var gist = clone_gist()
         gist.on('created', wrap(this.callback))
-        gist.create("contents of gist")
+        gist.create(sample_gist)
       },
 
-      "should receive a response": function (topic) {
-        assert.deepEqual(topic, response.post)
-      }
+      'should receive a response': ok(response.post)
     },
 
-    "without providing a password": {
+    'without providing a password': {
       topic: function () {
-        var gist = new Gist({ username: "octocat" })
+        var gist = clone_gist({ username: 'octocat' })
         gist.request = request
         gist.on('error:credentials', wrap(this.callback))
-        gist.create("contents of gist")
+        gist.create(sample_gist)
       },
 
-      "should receive an error": function (err) {
-        assert.equal(err, 'No OAuth token or username and password provided')
-      }
+      'should receive an error': error('No OAuth token or username and password provided')
     },
 
-    "providing just then token": {
+    'providing just then token': {
       topic: function () {
-        var gist = new Gist({ token: "abc123" })
+        var gist = clone_gist({ token: 'abc123' })
         gist.request = request
         gist.on('created', wrap(this.callback))
-        gist.create("contents of gist")
+        gist.create(sample_gist)
       },
 
-      "should receive a response": function (topic) {
-        assert.deepEqual(topic, response.post)
-      }
+      'should receive a response': ok(response.post)
     }
   }
 }).export(module)
